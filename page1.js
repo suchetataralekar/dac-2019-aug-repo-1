516bc8a994fa8df8b04d9714f9da47010390fa5b
sucheta <suchetataralekar@gmail.com>
2019-11-26 16:38:47 +0530

    Initial Commit

---
+++ page1.js
@@ -0,0 +1,14 @@
+var express=require("express");
+var mysql=require("mysql");
+
+var router = express();
+
+router.get("/emps", (request,responce)=>
+{
+    responce.send("Select method");
+})
+
+router.listen(9898,()=>
+{
+    console.log("Server started..");
+})
